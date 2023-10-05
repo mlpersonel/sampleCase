423141eb63ed2e9a3288e9ebbfe4acee470a6692
muhammedalilevent <technologiesssw@gmail.com>
2023-10-05 11:41:34 +0300

    project added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,9 +47,13 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    // LEVENT dependencies
+
+    // retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // eventbus
+    implementation("org.greenrobot:eventbus:3.3.1")
 
 
 }
---
+++ app/src/main/java/com/example/bookingapp/events/BookEvent.kt
@@ -0,0 +1,3 @@
+package com.example.bookingapp.events
+
+data class BookEvent(val isBooked:Boolean)
